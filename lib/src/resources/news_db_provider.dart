babe0a9dd8f55570d8c140512ececb137283601c
Sulabh Ghimire <sulabhghimire@pop-os.localdomain>
2023-05-07 19:50:23 +0545

    Just made newsDbProvider, newsApiProvider, repository, ItemModel and before refactor @

---
+++ lib/src/resources/news_db_provider.dart
@@ -0,0 +1,58 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path_provider/path_provider.dart';
+import 'dart:io';
+import 'package:path/path.dart';
+import 'dart:async';
+import '../models/item_model.dart';
+
+class NewsDbProvider {
+  late Database db; // connectioin to database on our device
+
+  void init() async {
+    Directory documentsDirectory = await getApplicationDocumentsDirectory();
+    final path = join(documentsDirectory.path, "items.db");
+    db = await openDatabase(
+      path,
+      version: 1,
+      onCreate: (Database newDB, int version) {
+        newDB.execute('''
+          CREATE TABLE Items
+            (
+              id INTEGER PRIMARY KEY,
+              type TEXT,
+              by TEXT,
+              time INTEGER,
+              text TEXT,
+              parent INTEGER,
+              kids BLOB,
+              dead INTEGER,
+              deleted INTEGER,
+              url TEXT,
+              score INTEGER,
+              title TEXT,
+              descendants INTEGER
+            )
+          ''');
+      },
+    );
+  }
+
+  Future<ItemModel?> fetchItem(int id) async {
+    final maps = await db.query(
+      "Items",
+      columns: null,
+      where: "id = ?",
+      whereArgs: [id],
+    );
+
+    if (maps.isNotEmpty) {
+      return ItemModel.fromDb(maps.first);
+    }
+
+    return null;
+  }
+
+  Future<int> addItem(ItemModel item) {
+    return db.insert("Items", item.toMapForDb());
+  }
+}
